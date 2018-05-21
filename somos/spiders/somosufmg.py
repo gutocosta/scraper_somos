0ac0b6d5de436ec7cecda7c70018f382e24aabb5
tkzhk <lucasvemerick@gmail.com>
2018-05-21 15:13:53 -0300

    nome e plavras chaves

--- somos/spiders/somosufmg.py
+++ somos/spiders/somosufmg.py
@@ -8,26 +8,30 @@ from somos.items import Prof
 class SomosUfmgSpider(CrawlSpider):
     #aqui e definido o nome da spider que sera chamado pelo comando "scrapy crawl -o output.xml NOME_DA_SPIDER"
     name = 'somosufmg'
-    #e definido a url que sera de inicio, ou seja, a pagina principal do site
-    #start_urls = ['http://www1.folha.uol.com.br/']
-
-    #as regras sao estabelecidas para restringir o scrapy aos links do cabecalho
-    #rules = [
-     #   Rule(LinkExtractor(restrict_xpaths= '//div[@class="l-header__nav c-site-nav"]'), callback= 'parse_sec'),
-    #]
     
     def start_requests(self):
+        #inicia-se um loop que percorre todas as paginas da pasta 'professores' 
     	for x in range(1, 4000):
+            #a cada pagia e chamada a funcao para parsear o item
         	yield scrapy.Request("http://somos.ufmg.br/professores/view/%d" % (x),self.parse_item)
 
     def parse_item(self, response):
+        # uma lista que recebe todos os elementos do item e criada
         litem = []
+
+        # i e definido como o item Prof cuja classa e definida no arquivo items
         i = Prof()
+        #o nome do professor e extraido a partir da sua identificacao na pagina 
         litem.append(response.xpath('//div[@class="span8"]/h2/text()').extract_first())
+
+        #desc recebe todas as palavras chaves listadas na pagina
         desc = response.xpath('//div[@class="span6 cloud palavrasChave"]/a')
+
+        #cada palavra chave presente em desc e extraida 
         for item in desc:
         	litem.append(item.xpath('./span/text()').extract_first())
 
+        #o item Prof recebe a lista convertida para string
         i['content'] = ",".join(litem)
 
         yield i
